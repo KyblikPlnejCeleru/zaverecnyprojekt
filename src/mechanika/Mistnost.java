ffbb292e1e1f8a9aeadb485e2cc3cedff0dc0f9b
Romek domek <potocek@spsejecna.cz>
2026-01-21 18:34:13 +0100

    pridal jsem json a nacitani z jsonu

--- src/mechanika/Mistnost.java
+++ src/mechanika/Mistnost.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Mistnost {
     private String nazev;
     private String popis;
+    private int id;
     private ArrayList<Mistnost> vstupy;
     private ArrayList<Predmet> predmety;
     private ArrayList<Postava> postavy;
@@ -26,6 +27,46 @@ public class Mistnost {
     public Predmet seberPredmet(String nazev) {
         // TODO
         return null;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public void setPopis(String popis) {
+        this.popis = popis;
+    }
+
+    public ArrayList<Mistnost> getVstupy() {
+        return vstupy;
+    }
+
+    public void setVstupy(ArrayList<Mistnost> vstupy) {
+        this.vstupy = vstupy;
+    }
+
+    public ArrayList<Predmet> getPredmety() {
+        return predmety;
+    }
+
+    public void setPredmety(ArrayList<Predmet> predmety) {
+        this.predmety = predmety;
+    }
+
+    public ArrayList<Postava> getPostavy() {
+        return postavy;
+    }
+
+    public void setPostavy(ArrayList<Postava> postavy) {
+        this.postavy = postavy;
     }
 }
 
