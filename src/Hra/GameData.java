deafa32d820bf8c70e0952a2b2a01e482acafdbe
Romek domek <potocek@spsejecna.cz>
2026-01-22 14:22:27 +0100

    Udelany protopit prikazu jdi.

---
+++ src/Hra/GameData.java
@@ -0,0 +1,71 @@
+package Hra;
+import com.sun.tools.javac.Main;
+import mechanika.Mistnost;
+import mechanika.Postava;
+import com.google.gson.Gson;
+import mechanika.Predmet;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+/**
+ * Represents the game data loaded from a JSON file.
+ * This class serves as a data container for all static game content, such as items, characters, locations, and quests.
+ *
+ */
+public class GameData {
+
+    public ArrayList<Predmet> Predmety;
+    public ArrayList<Postava> postavy;
+    public ArrayList<Mistnost> locations;
+
+    /**
+     * Loads game data from a JSON file.
+     * @param resourcePath path to the resource file
+     * @return a GameData object filled with the loaded data
+     */
+    public static GameData loadGameDataFromResources(String resourcePath) {
+        //Vytvoření objektu pro práci s JSON souborem
+        Gson gson = new Gson();
+
+        //Načtení souboru gamedata.json, musí být ve složce res/resources, ta musí být označena jako resource složka projektu
+        try (InputStream is = Main.class.getResourceAsStream(resourcePath)) {
+
+            //Zde ověřujeme, zdali soubor existuje
+            if (is == null) {
+                throw new IllegalStateException("Nenalezen resource: " + resourcePath +
+                        " (zkontrolujte, že soubor je v src/main/resources).");
+            }
+
+            //Přečte celý JSON a vytvoří instanci GameData, naplní vlastnosti podle názvů klíčů v JSONU, vrátí se hotová třída GameData
+            return gson.fromJson(
+                    new InputStreamReader(is, StandardCharsets.UTF_8),
+                    GameData.class
+            );
+
+        } catch (Exception e) {
+            throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
+        }
+
+    }
+
+    /**
+     * Finds a specific location by its identifier.
+     * @param nazev the identifier of the location to be found
+     * @return the matching location
+     */
+    public  Mistnost findLocation(String nazev) {
+        for (Mistnost l : locations) {
+            if (l.getNazev().equals(nazev)){
+                return l;
+            }
+        }
+        throw new IllegalArgumentException("Neexistuje lokace jmenem: " + nazev);
+    }
+
+    public ArrayList<Mistnost> getLocations() {
+        return locations;
+    }
+}
--- src/prikazy/Jdi.java
+++ src/prikazy/Jdi.java
@@ -1,10 +1,26 @@
 package prikazy;
 
+import Hra.GameData;
+import mechanika.Hrac;
+
 public class Jdi implements Prikaz {
 
+    private GameData hra;
+    private Hrac hrac;
+
     @Override
-    public String execute() {
+    public String execute(String vstup) {
+        for (int i = 0; i < hra.locations.size(); i++) {
+        if (vstup.contains(hra.locations.get(i).getNazev())){
+         hrac.setAktualniMistnost(hra.locations.get(i));
+        }
+        }
         return "";//TODO
+    }
+
+    public Jdi(GameData hra, Hrac hrac) {
+        this.hra = hra;
+        this.hrac=hrac;
     }
 
     @Override
