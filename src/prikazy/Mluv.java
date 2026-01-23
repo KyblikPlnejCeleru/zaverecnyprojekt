3d73349b4a1f02c1117a05857b71cbd1e5b5a484
RADIM <cunikov@rusko.mail>
2026-01-23 09:41:59 +0100

    prikaz k mluveni s postavami

--- src/prikazy/Mluv.java
+++ src/prikazy/Mluv.java
@@ -1,9 +1,20 @@
 package prikazy;
 
+import Hra.GameData;
+import mechanika.Hrac;
+
+import java.util.Locale;
+
 public class Mluv implements Prikaz {
+    GameData g;
+    Hrac h;
     @Override
     public String konej(String vstup) {
-        return "";
+        for (int i = 0; i <h.getAktualniMistnost().getPostavy().size() ; i++) {
+            if (h.getAktualniMistnost().getPostavy().equals(vstup.toLowerCase().trim())){
+                return h.getAktualniMistnost().getPostavy().get(i).mluv();
+            }
+        }return "";
     }
 
     @Override
