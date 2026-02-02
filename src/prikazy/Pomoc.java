c6b7e1f1f20d13d97dc29be9c4dd4260ab891669
potocek <labubu@hotmail.com>
2026-02-02 13:01:29 +0100

    TXT ve kterym je pomoc k prikazum :]

---
+++ src/prikazy/Pomoc.java
@@ -0,0 +1,15 @@
+package prikazy;
+
+public class Pomoc implements Prikaz{
+
+
+    @Override
+    public String konej(String vstup) {
+        return "";
+    }
+
+    @Override
+    public boolean ukonceni() {
+        return false;
+    }
+}
