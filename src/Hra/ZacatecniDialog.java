6372ecc70f68dadea4c8e63d349897bcd3259341
Romek domek <potocek@spsejecna.cz>
2026-02-21 19:39:38 +0100

    Stal se ze mne spisovatel {dodelal jsem dzavadoks]

--- src/Hra/ZacatecniDialog.java
+++ src/Hra/ZacatecniDialog.java
@@ -6,7 +6,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Trida ktera slouzi pro nacitani a zpracovani zacatecniho dialogu >]
+ * @author romek studios
+ */
+
 public class ZacatecniDialog {
+
+    /**
+     * Metoda pro nacteni a vypsani toho dialogu z textoveho souboru kterej je typu .txt {to uz je funfact}
+     * @return vraci ten dialog bro
+     */
     public String dialog() {
         StringBuilder vystup = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader("res/dialog.txt"))) {
